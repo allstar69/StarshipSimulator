70fad75d211ea3ae52329ff1acb03366a2337927
stephensjoshua <jstephens@student.neumont.edu>
2015-03-04 17:56:41 -0700

    Fixed Screen click bug
    I fixed the bug that froze the game when you clicked the screen.

--- src/com/starshipsim/application/Game.java
+++ src/com/starshipsim/application/Game.java
@@ -50,6 +50,7 @@ public class Game extends JFrame {
 		
 		this.keyboard = new KeyboardListener();
 		this.addKeyListener(keyboard);
+		canvas.addKeyListener(keyboard);
 		
 		states = new StateManager(keyboard);
 	}
