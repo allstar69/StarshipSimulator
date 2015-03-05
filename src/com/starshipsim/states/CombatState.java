c0263c5eaabf081a6dfeb4ecd95e7565292b44ac
allstar69 <allstar602@gmail.com>
2015-03-05 10:29:25 -0700

    Updated menus using the new MenuUI

--- src/com/starshipsim/states/CombatState.java
+++ src/com/starshipsim/states/CombatState.java
@@ -2,27 +2,45 @@ package com.starshipsim.states;
 
 import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 
 import com.starshipsim.files.FileIO;
 import com.starshipsim.listeners.KeyboardListener;
+import com.starshipsim.panels.MenuUI;
 import com.sun.glass.events.KeyEvent;
 
 public class CombatState extends State {
 
-	private static Image background = FileIO.loadImage("resources/space.png");
+	private static Image imgBackground = FileIO.loadImage("resources/space.png");
+	private static Image imgMenu = FileIO.loadImage("resources/smallmenu.png");
+	private static Image shipCursor = FileIO.loadImage("resources/smallship1.png");
 	
 	private KeyboardListener keyboard;
+	
+	private MenuUI menu;
+	
+	private String[] list;
+	
+	int currentOption = 0;
 	
 	public CombatState(StateManager manager) {
 		super(manager);
 		this.keyboard = manager.getKeyboard();
+		initialize();
 	}
 
 	@Override
 	public void initialize() {
+		list = new String[] {
+			"Attack",
+			"Items",
+			"Defend",
+			"Run"
+		};
 		
+		menu = new MenuUI(manager.getKeyboard(), imgMenu, shipCursor, list);
 	}
 
 	@Override
@@ -30,14 +48,27 @@ public class CombatState extends State {
 		if(keyboard.keyDownOnce(KeyEvent.VK_ESCAPE)) {
 			manager.popState();
 		}
+		
+		menu.update();
+		currentOption = menu.getCurrentOption();
 	}
 
 	@Override
 	public void draw(Graphics g, Canvas canvas) {
-		g.drawImage(background, 0, 0, null);
+		g.drawImage(imgBackground, 0, 0, null);
 		
 		g.setColor(Color.white);
 		g.drawString("Press Escape to return the Map.", 32, 32);
+		
+		g.setFont(new Font("Showcard Gothic", Font.ITALIC, 24));
+		
+		int menuX = (canvas.getWidth()/2) - (imgMenu.getWidth(null)/2);
+		int menuY = (canvas.getHeight()/2);
+		
+		menu.setX(menuX);
+		menu.setY(menuY);
+		
+		menu.draw(g);
 	}
 
 	@Override
--- src/com/starshipsim/states/MainMenuState.java
+++ src/com/starshipsim/states/MainMenuState.java
@@ -2,18 +2,18 @@ package com.starshipsim.states;
 
 import java.awt.Canvas;
 import java.awt.Font;
-import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.KeyEvent;
 
 import com.starshipsim.files.FileIO;
 import com.starshipsim.listeners.KeyboardListener;
+import com.starshipsim.panels.MenuUI;
 
 public class MainMenuState extends State {
 
 	private static Image background = FileIO.loadImage("resources/space.png");
-	private static Image menu = FileIO.loadImage("resources/largemenu.png");
+	private static Image largeMenu = FileIO.loadImage("resources/largemenu.png");
 	private static Image shipCursor = FileIO.loadImage("resources/smallship1.png");
 	
 	private int currentOption = 0;
@@ -24,6 +24,8 @@ public class MainMenuState extends State {
 			"Exit",
 	};
 	
+	private MenuUI menu;
+	
 	public MainMenuState(StateManager manager) {
 		super(manager);
 		initialize();
@@ -31,23 +33,15 @@ public class MainMenuState extends State {
 	
 	@Override
 	public void initialize() {
+		menu = new MenuUI(manager.getKeyboard(), largeMenu, shipCursor, list);
 	}
 
 	@Override
 	public void update() {
 		KeyboardListener keyboard = manager.getKeyboard();
 		
-		if(keyboard.keyDownOnce(KeyEvent.VK_S)) {
-			currentOption++;
-		} else if(keyboard.keyDownOnce(KeyEvent.VK_W)) {
-			currentOption--;
-		}
-		
-		if(currentOption < 0) {
-			currentOption = list.length-1;
-		} else if(currentOption > list.length-1) {
-			currentOption = 0;
-		}
+		menu.update();
+		this.currentOption = menu.getCurrentOption();
 		
 		if(keyboard.keyDownOnce(KeyEvent.VK_ENTER)) {
 			switch(currentOption) {
@@ -66,20 +60,15 @@ public class MainMenuState extends State {
 	public void draw(Graphics g, Canvas canvas) {
 		g.drawImage(background, 0, 0, null);
 		
-		int menuX = (canvas.getWidth()/2) - (menu.getWidth(null)/2);
-		int menuY = (canvas.getHeight()/2) - (menu.getHeight(null)/2);
-		g.drawImage(menu, menuX, menuY, null);
-		
 		g.setFont(new Font("Showcard Gothic", Font.ITALIC, 72));
-		FontMetrics metrics = g.getFontMetrics();
 		
-		int x = (menuX + (menu.getWidth(null)/2)) - metrics.stringWidth(list[0])/2;
-		for (int i = 0; i < list.length; i++) {
-			int y = menuY + (100 + (i * 90));
-			g.drawString(list[i], x, y);
-		}
+		int menuX = (canvas.getWidth()/2) - (largeMenu.getWidth(null)/2);
+		int menuY = (canvas.getHeight()/2) - (largeMenu.getHeight(null)/2);
 		
-		g.drawImage(shipCursor, (menuX + menu.getWidth(null)/2 - metrics.stringWidth(list[0])/2) - shipCursor.getWidth(null), menuY + (50 + (currentOption * 90)), null);
+		menu.setX(menuX);
+		menu.setY(menuY);
+		
+		menu.draw(g);
 	}
 
 	@Override
