01fbf7e3fc8cf4c88d32939d147fcef6c0407e8b
rstrasdin <rstrasdin@student.neumont.edu>
2015-03-06 09:28:06 -0700

    Updated Reward
    Reward now holds an item that is randomly generated to one of the three
    items in the game.

--- src/com/starshipsim/rewards/Reward.java
+++ src/com/starshipsim/rewards/Reward.java
@@ -1,9 +1,28 @@
 package com.starshipsim.rewards;
 
-import com.starshipsim.items.Item;
+import java.util.Random;
+
+import com.starshipsim.items.*;
 
 public class Reward {
 	private Item rewardItem;
+	
+	public Reward(){
+		Random ran = new Random();
+		int num = ran.nextInt(3);
+		switch(num){
+		case 0:
+			setRewardItem(new ItemFuel());
+			break;
+		case 1:
+			setRewardItem(new ItemRepairDrone());
+			break;
+			
+		case 2:
+			setRewardItem(new ItemStunBomb());
+			break;		
+		}
+	}
 
 	public Item getRewardItem() {
 		return rewardItem;
