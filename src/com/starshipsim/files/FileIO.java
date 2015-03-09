9afa33fa2093b0c73ec9b31584799eb50c1999fc
allstar69 <allstar602@gmail.com>
2015-03-09 10:42:50 -0600

    Serialization Methods
    This will be used later for saving the game

--- src/com/starshipsim/files/FileIO.java
+++ src/com/starshipsim/files/FileIO.java
@@ -2,7 +2,11 @@ package com.starshipsim.files;
 
 import java.awt.Image;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 import javax.imageio.ImageIO;
 
@@ -19,6 +23,37 @@ public class FileIO {
 		}
 
 		return image;
+	}
+	
+	public static void serialize(Object o) {
+		try {
+			FileOutputStream fileOut = new FileOutputStream("object.ser");
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+
+			out.writeObject(o);
+			out.close();
+			fileOut.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Object deserialize(File file) {
+		Object o = null;
+		
+		try {
+			FileInputStream fileIn = new FileInputStream(file);
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+
+			o = in.readObject();
+			in.close();
+			fileIn.close();
+		} catch (IOException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return o;
 	}
 
 }
