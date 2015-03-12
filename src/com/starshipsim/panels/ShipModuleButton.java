3d9725716d8fd202203b66dfe57bfc6319a606bb
allstar69 <allstar602@gmail.com>
2015-03-11 23:08:40 -0600

    Added description boxes to the Ship Module Menu
    Highlighting over the buttons in the Ship Builder State show
    descriptions.

--- src/com/starshipsim/panels/ShipModuleButton.java
+++ src/com/starshipsim/panels/ShipModuleButton.java
@@ -1,12 +1,16 @@
 package com.starshipsim.panels;
 
+import java.awt.Canvas;
+
 import com.starshipsim.graphics.ButtonUI;
 import com.starshipsim.listeners.GameMouseListener;
 import com.starshipsim.shipmodules.ShipModule;
+import com.starshipsim.ui.DescriptionBoxUI;
 
 public abstract class ShipModuleButton extends ButtonUI {
 
 	private ShipModule module;
+	private DescriptionBoxUI desc;
 	
 	public ShipModule getModule() {
 		return module;
@@ -16,9 +20,24 @@ public abstract class ShipModuleButton extends ButtonUI {
 		this.module = module;
 	}
 
-	public ShipModuleButton(int x, int y, int width, int height, GameMouseListener mouse, ShipModule module) {
+	public ShipModuleButton(int x, int y, int width, int height, GameMouseListener mouse, ShipModule module, DescriptionBoxUI desc) {
 		super(module.getName(), x, y, width, height, mouse);
 		this.module = module;
+		this.desc = desc;
+	}
+	
+	@Override
+	public void update(Canvas canvas) {
+		super.update(canvas);
+		
+		if(this.mouseInside(canvas)) {
+			desc.getDesc().clear();
+			
+			desc.setVisible(true);
+			
+			desc.getDesc().add(module.getName());
+			desc.getDesc().add("Quality: " + module.getQuality().toString());
+		}
 	}
 
 }
--- src/com/starshipsim/panels/ShipModuleMenuPanel.java
+++ src/com/starshipsim/panels/ShipModuleMenuPanel.java
@@ -2,6 +2,7 @@ package com.starshipsim.panels;
 
 import java.awt.Canvas;
 import java.awt.Graphics;
+import java.awt.Point;
 import java.util.ArrayList;
 
 import com.starshipsim.entities.Player;
@@ -10,14 +11,18 @@ import com.starshipsim.interfaces.Drawable;
 import com.starshipsim.items.Item;
 import com.starshipsim.shipmodules.ShipModule;
 import com.starshipsim.states.StateManager;
+import com.starshipsim.ui.DescriptionBoxUI;
 
 public class ShipModuleMenuPanel implements Drawable {
 
 	private ArrayList<ShipModule> modules;
 	private ArrayList<ButtonUI> buttons;
+	private DescriptionBoxUI desc;
 	
 	public ShipModuleMenuPanel(StateManager manager, Player player) {		
 		fetchModulesInventory(player);
+		this.desc = new DescriptionBoxUI(500, 500, 250, 150);
+		this.desc.setVisible(false);
 		initializeButtons(manager);
 	}
 	
@@ -27,7 +32,7 @@ public class ShipModuleMenuPanel implements Drawable {
 		int x = 800;
 		int y = 50;
 		for (int i = 0; i < modules.size(); i++) {
-			ShipModuleButton button = new ShipModuleButton(x, y + (i*125), 300, 100, manager.getMouse(), modules.get(i)) {
+			ShipModuleButton button = new ShipModuleButton(x, y + (i*125), 300, 100, manager.getMouse(), modules.get(i), desc) {
 				@Override
 				public void clicked() {
 				}
@@ -47,11 +52,35 @@ public class ShipModuleMenuPanel implements Drawable {
 		}
 	}
 	
+	public void updateDescBox(Canvas canvas) {
+		Point point = canvas.getMousePosition();
+		if(point != null) {
+			int mouseX = point.x;
+			int mouseY = point.y;
+			
+			desc.setX(mouseX);
+			desc.setY(mouseY);
+		}
+		
+		desc.update(canvas);
+	}
+	
 	@Override
 	public void update(Canvas canvas) {
+		int count = 0;
 		for (ButtonUI buttonUI : buttons) {
 			buttonUI.update(canvas);
+			
+			if(!buttonUI.mouseInside(canvas)) {
+				count++;
+			}
 		}
+		
+		if(count == buttons.size()) {
+			desc.setVisible(false);
+		}
+		
+		this.updateDescBox(canvas);
 	}
 
 	@Override
@@ -59,6 +88,8 @@ public class ShipModuleMenuPanel implements Drawable {
 		for (ButtonUI buttonUI : buttons) {
 			buttonUI.draw(g);
 		}
+		
+		desc.draw(g);
 	}
 
 }
