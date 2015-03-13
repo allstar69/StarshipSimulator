e674e8603ef9dd71933704ad1c68cac7b0f70517
rstrasdin <rstrasdin@student.neumont.edu>
2015-03-13 10:50:49 -0600

    Random Damage for Obstacles
    Damage is a random amount between 0 and 20% of the current hull
    durability.

--- src/com/starshipsim/obstacles/Sabotage.java
+++ src/com/starshipsim/obstacles/Sabotage.java
@@ -1,8 +1,11 @@
 package com.starshipsim.obstacles;
 
+import java.util.Random;
+
 import com.starshipsim.entities.Player;
 
 public class Sabotage extends Obstacle {
+	private int realDamage;
 	
 	public Sabotage(int amount){
 		
@@ -10,12 +13,26 @@ public class Sabotage extends Obstacle {
 	}
 	
 	public void run(Player play){
+		Random ran = new Random();
+		double percentToBeSabotaged = ran.nextInt(20);
+			percentToBeSabotaged = percentToBeSabotaged / 100;
+		double damageAmount = percentToBeSabotaged * (play.getShip().getDurability());
+		setRealDamage((int)damageAmount);
 		
-		play.getShip().setDurability(play.getShip().getDurability() - 5); //sets the ships durability to 5 below what it is.
-		
+		play.getShip().setDurability(play.getShip().getDurability() - getRealDamage()); //sets the ships durability to 5 below what it is.
+
 	}
 	public String toString(){
-		return "You have been sabotaged and your hull lost 5 durability.";
+		return "You have been sabotaged and your hull lost " + getRealDamage() + " durability.";
 	}
+
+	public int getRealDamage() {
+		return realDamage;
+	}
+
+	public void setRealDamage(int realDamage) {
+		this.realDamage = realDamage;
+	}
+	
 
 }
